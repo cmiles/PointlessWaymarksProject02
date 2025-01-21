53370671579a48291acccedc10b8356e8e59efd8
charl <charl@noemail.net>
2025-01-21 16:54:59 +0000

    Update PublishReadmeHelper and include helpers for GitHub README.md mirror files
    FossilOrigin-Name: 8886f8e2ba721c1d41f3288b20348b788ea4a229a36893ec327d49eef6a076d2

--- Tools-CopyReadmeToProjectSpecificReadme.ps1
+++ Tools-CopyReadmeToProjectSpecificReadme.ps1
@@ -2,17 +2,7 @@ $ErrorActionPreference = "Stop"
 
 if (-not ("BuildTools\PointlessWaymarks.PublishReadmeHelper.exe" | Test-Path)) {
 	
-	$vsWhere = "{0}\Microsoft Visual Studio\Installer\vswhere.exe" -f ${env:ProgramFiles(x86)}
-
-	$msBuild = & $vsWhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
-
-	dotnet clean .\PointlessWaymarks.PublishReadmeHelper\PointlessWaymarks.PublishReadmeHelper.csproj -property:Configuration=Release -property:Platform=x64 -verbosity:minimal
-
-	dotnet restore .\PointlessWaymarks.PublishReadmeHelper\PointlessWaymarks.PublishReadmeHelper.csproj -r win-x64 -verbosity:minimal
-
-	& $msBuild .\PointlessWaymarks.PublishReadmeHelper\PointlessWaymarks.PublishReadmeHelper.csproj -t:publish -p:PublishProfile=.\PointlessWaymarks.PublishReadmeHelper\Properties\PublishProfile\FolderProfile.pubxml -verbosity:minimal
-
-	if ($lastexitcode -ne 0) { throw ("Exec: " + $errorMessage) }
+	.\Publish-PublishReadmeHelper.ps1
 	
 }
 
