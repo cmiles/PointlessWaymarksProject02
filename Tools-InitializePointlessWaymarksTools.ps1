6e13b148959df6dfe34a566f772ba7f24dae64d7
charl <>
2025-12-22 16:23:46 +0000

    Update the PointlessWaymarksTools init script for the new local focused fossil setup.
    FossilOrigin-Name: 5a21db89c20039a34d2c1b02a0f03d6b20ffff8ffd5a7a9143cc5c7a09d9d64a

--- Tools-InitializePointlessWaymarksTools.ps1
+++ Tools-InitializePointlessWaymarksTools.ps1
@@ -4,10 +4,10 @@ $userName = Read-Host "Enter your username (leave blank if none)"
 # Check if the username is provided
 if ($userName) {
     # Run the fossil clone command with the username
-    fossil clone https://$userName@chiselapp.com/user/cmiles/repository/pointless-waymarks-tools pointless-waymarks-tools.fossil
+    fossil clone http://$userName@localhost:8080/pointless-waymarks-tools pointless-waymarks-tools.fossil
 } else {
     # Run the fossil clone command without the username
-    fossil clone https://chiselapp.com/user/cmiles/repository/pointless-waymarks-tools pointless-waymarks-tools.fossil
+    fossil clone http://localhost:8080/user/cmiles/repository/pointless-waymarks-tools pointless-waymarks-tools.fossil
 }
 
 # Define the target directory
